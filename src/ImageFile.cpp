f72b37b03d7479b388fa51392ba0144976c3be4f
Cole Brinsfield <brinsfield.cole@gmail.com>
2023-02-03 01:14:10 -0800

    removing stb_image_write reference

--- src/ImageFile.cpp
+++ src/ImageFile.cpp
@@ -7,8 +7,8 @@
 #include "stb_image.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+//#define STB_IMAGE_WRITE_IMPLEMENTATION
+//#include "stb_image_write.h"
 
 ImageFile::ImageFile()
 {
